0100ffdb6d71eaf996be618c5b84af3a29aa6c68
nana <eyzzz_@naver.com>
2021-11-12 14:28:31 +0900

    커밋 못한대소,,,

--- freeboard_frontend/pages/_app.js
+++ freeboard_frontend/pages/_app.js
@@ -1,22 +1,17 @@
-import '../styles/globals.css'
-import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
-
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   const client = new ApolloClient({
-    uri: 'http://backend04.codebootcamp.co.kr/graphql',
-    cache: new InMemoryCache()
-
-  })
+    uri: "http://backend04.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache(),
+  });
 
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
-      </ApolloProvider>
-    
-
-  )
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
-
+export default MyApp;
