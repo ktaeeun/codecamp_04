72b6a1814489a4212957ecb154b2d84bf4d26aa2
nana <eyzzz_@naver.com>
2021-11-17 09:51:23 +0900

    코멘트 띄우기

--- freeboard_frontend/pages/_app.tsx
+++ freeboard_frontend/pages/_app.tsx
@@ -1,7 +1,9 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import "../styles/globals.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { AppProps } from "next/dist/shared/lib/router/router";
+import "antd/dist/antd.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   const client = new ApolloClient({
     uri: "http://backend04.codebootcamp.co.kr/graphql",
     cache: new InMemoryCache(),
--- freeboard_frontend/pages/boards/[boardId]/index.tsx
+++ freeboard_frontend/pages/boards/[boardId]/index.tsx
@@ -1,9 +1,13 @@
 import BoardDetail from "../../../src/components/board/detail/BoardDetail.container";
+import BoardCommentList from "../../../src/components/BoardComment/list/BoardCommentList.container";
+import BoardCommentWrite from "../../../src/components/BoardComment/write/BoardCommentWrite.container";
 
 export default function BoardDetailUI() {
   return (
     <>
       <BoardDetail />
+      <BoardCommentWrite />
+      <BoardCommentList />
     </>
   );
 }
