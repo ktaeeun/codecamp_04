5a60c9c46feb6e0d94182eba17cf8cc1081eedb6
nana <eyzzz_@naver.com>
2021-11-17 17:36:49 +0900

    레이아웃 업뎃

---
+++ class/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { Global } from "@emotion/react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import "antd/dist/antd.css";
+import { AppProps } from "next/dist/shared/lib/router/router";
+import Layout from "../src/components/commons/layout";
+import { globalStyles } from "../src/commons/styles/globalStyles";
+function MyApp({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: "http://example.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Global styles={globalStyles} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
+  );
+}
+
+export default MyApp;
