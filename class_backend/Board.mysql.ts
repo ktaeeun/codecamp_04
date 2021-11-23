2e8db882f3f2d8404c4e9b0fd8d05d7f2a78a30c
nana <eyzzz_@naver.com>
2021-11-23 18:07:09 +0900

    커밋

---
+++ class_backend/Board.mysql.ts
@@ -0,0 +1,16 @@
+import { BaseEntity, Entity, Column, PrimaryGeneratedColumn } from "typeorm"; // typeorm에게 테이블을 만들어달라고 요청해야함
+
+@Entity()
+export default class Board extends BaseEntity {
+  @PrimaryGeneratedColumn("increment")
+  number!: number;
+
+  @Column({ type: "text" })
+  writer!: String;
+
+  @Column({ type: "text" })
+  title!: String;
+
+  @Column({ type: "integer" })
+  age!: number;
+}
