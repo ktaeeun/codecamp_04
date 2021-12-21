5aaaf5614ea25bcbe45783ab865e043255202676
nana <eyzzz_@naver.com>
2021-12-21 13:04:13 +0900

    update

--- class_build/pages/boards/index.js
+++ class_build/pages/boards/index.js
@@ -6,8 +6,8 @@ export default function BoardsPage() {
         <meta property="og:title" content="나만의 사이트 게시판" />
         <meta property="og:url" content="http://withnyang.shop" />
         <meta
-          property="og:img"
-          content="https://i.ytimg.com/vi/vf8t8LgZj74/maxresdefault.jpg"
+          property="og:image"
+          content="https://cdn.crowdpic.net/list-thumb/thumb_l_F849A239E3EC8D949EB01552E25497E0.jpg"
         />
         <meta property="og:description" content="삼색이 보고가세요" />
       </Head>
